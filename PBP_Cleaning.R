a39b3cd612f806d6cfddf052e1e54b2945ccb672
Andrew Noecker <noecke2@stolaf.edu>
2022-11-20 14:38:27 -0600

    Updated lineup and PBP cleaning, started building report with individual and 2 player lineup summaries

---
+++ PBP_Cleaning.R
@@ -0,0 +1,37 @@
+
+# File to Clean PBP Data --------------------------------------------------
+
+
+
+# Load in libraries -------------------------------------------------------
+
+
+knitr::opts_chunk$set(echo = TRUE)
+library(readxl)
+library(readr)
+library(lubridate)
+library(tidyverse)
+
+
+
+
+
+# Function to Clean PBP Data File -----------------------------------------
+
+clean_pbp <- function(filename){
+  new_tibble <- read_excel(str_c("data/", filename, ".xlsx"))
+  new_data <- new_tibble%>%
+    select(number, 114:124, date, visname, homename)%>%
+    filter(!is.na(action))%>%
+    rename(quarter = number)%>%
+    mutate(date = mdy(date))
+  new_data
+}
+
+
+
+# Game by game PBP data ---------------------------------------------------
+
+pbp <- clean_pbp("11_11_Alma_PBP") %>% 
+  bind_rows(clean_pbp("11_12_North_Park_PBP"))
+
