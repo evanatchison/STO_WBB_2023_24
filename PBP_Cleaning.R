2752230cccdcc825af6e051ba6fe1559b30f4739
Andrew Noecker <noecke2@stolaf.edu>
2022-12-11 17:26:44 -0600

    PBP data through 12/7, MIAC game report created and relevant files (lineup_cleaning and PBP_cleaning) updated accordingly

--- PBP_Cleaning.R
+++ PBP_Cleaning.R
@@ -19,7 +19,7 @@ library(tidyverse)
 # Function to Clean PBP Data File -----------------------------------------
 
 clean_pbp <- function(filename){
-  new_tibble <- read_excel(str_c("data/", filename, ".xlsx"))
+  new_tibble <- read_excel(str_c("data/PBP/", filename, ".xlsx"))
   new_data <- new_tibble%>%
     select(number, 114:124, date, visname, homename)%>%
     filter(!is.na(action))%>%
@@ -33,5 +33,10 @@ clean_pbp <- function(filename){
 # Game by game PBP data ---------------------------------------------------
 
 pbp <- clean_pbp("11_11_Alma_PBP") %>% 
-  bind_rows(clean_pbp("11_12_North_Park_PBP"))
+  bind_rows(clean_pbp("11_12_North_Park_PBP"),
+            clean_pbp("11_16_Minnesota_Morris_PBP"),
+            clean_pbp("11_19_St_Bens_PBP"),
+            clean_pbp("11_30_St_Marys_PBP"),
+            clean_pbp("12_3_Hamline_PBP"),
+            clean_pbp("12_7_Gustavus_PBP"))
 
